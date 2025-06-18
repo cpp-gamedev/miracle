d87ec640dd3afb5968898614a69ac64c27f76c88
NikolaJelic <jelicnikola18@gmail.com>
2025-06-18 22:04:45 +0200

    Refacotr PR requests - rotate lighthouse image so it better follows the cursor - adjust whitespace - fix unit vector typo

--- src/lighhouse.hpp
+++ src/lighhouse.hpp
@@ -6,19 +6,20 @@
 #include <le2d/event.hpp>
 #include <le2d/renderer.hpp>
 #include <le2d/service_locator.hpp>
-#include "glm/ext/vector_float2.hpp"
-#include "gsl/pointers"
+#include <optional>
 #include "le2d/texture.hpp"
+
 namespace miracle {
 class Lighthouse {
   public:
 	explicit Lighthouse(gsl::not_null<le::ServiceLocator const*> services);
-	void rotate_towards_cursor(glm::vec2 const& cursor_pos);
+
+	void rotate_towards_cursor(glm::vec2 cursor_pos);
 	void render(le::Renderer& renderer) const;
 
   private:
-	le::drawable::Circle m_sprite{};
-	std::optional<le::Texture> m_texture;
 	gsl::not_null<le::ServiceLocator const*> m_services;
+	std::optional<le::Texture> m_texture;
+	le::drawable::Circle m_sprite{};
 };
 } // namespace miracle
--- src/lighthouse.cpp
+++ src/lighthouse.cpp
@@ -3,7 +3,6 @@
 #include "le2d/data_loader.hpp"
 
 namespace miracle {
-
 Lighthouse::Lighthouse(gsl::not_null<le::ServiceLocator const*> services) : m_services(services) {
 	m_sprite.create(200.0f);
 
@@ -13,12 +12,13 @@ Lighthouse::Lighthouse(gsl::not_null<le::ServiceLocator const*> services) : m_se
 	m_texture = asset_loader.load_texture("images/lighthouse.png");
 	m_sprite.texture = &m_texture.value();
 }
-void Lighthouse::rotate_towards_cursor(glm::vec2 const& cursor_pos) {
+
+void Lighthouse::rotate_towards_cursor(glm::vec2 cursor_pos) {
 	auto const dist_sq = glm::length2((cursor_pos));
 	if (dist_sq > 0.1f) {
 		auto const dist = std::sqrt(dist_sq);
 		auto const normalized = cursor_pos / dist;
-		static constexpr auto up_v = glm::vec2(0.01f, 1.0f);
+		static constexpr auto up_v = glm::vec2(0.0f, 1.0f);
 		auto const dot = glm::dot(normalized, up_v);
 		auto const angle = glm::degrees(std::acos(dot));
 		m_sprite.transform.orientation = cursor_pos.x > 0.0f ? -angle : angle;
